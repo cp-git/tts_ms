9d16e7ed6ed3b524bac3766159fcd93e6961126a
Kaushik <kaushikwalke6565@gmail.com>
2023-09-12 17:59:52 +0530

    isAdmin

--- services/employee/src/main/java/com/cpa/ttsms/dto/EmployeeAndEmployeePhotosDTO.java
+++ services/employee/src/main/java/com/cpa/ttsms/dto/EmployeeAndEmployeePhotosDTO.java
@@ -20,146 +20,91 @@ public class EmployeeAndEmployeePhotosDTO {
 	private int photoId;
 
 	private String photoFilename;
+	
+	private boolean admin;
 
-	/**
-	 * @return the employeeId
-	 */
 	public int getEmployeeId() {
 		return employeeId;
 	}
 
-	/**
-	 * @param employeeId the employeeId to set
-	 */
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
 
-	/**
-	 * @return the countryId
-	 */
 	public int getCountryId() {
 		return countryId;
 	}
 
-	/**
-	 * @param countryId the countryId to set
-	 */
 	public void setCountryId(int countryId) {
 		this.countryId = countryId;
 	}
 
-	/**
-	 * @return the companyId
-	 */
 	public int getCompanyId() {
 		return companyId;
 	}
 
-	/**
-	 * @param companyId the companyId to set
-	 */
 	public void setCompanyId(int companyId) {
 		this.companyId = companyId;
 	}
 
-	/**
-	 * @return the firstName
-	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
-	/**
-	 * @param firstName the firstName to set
-	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	/**
-	 * @return the lastName
-	 */
 	public String getLastName() {
 		return lastName;
 	}
 
-	/**
-	 * @param lastName the lastName to set
-	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	/**
-	 * @return the birthDate
-	 */
 	public Date getBirthDate() {
 		return birthDate;
 	}
 
-	/**
-	 * @param birthDate the birthDate to set
-	 */
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
 
-	/**
-	 * @return the employeeEmail
-	 */
 	public String getEmployeeEmail() {
 		return employeeEmail;
 	}
 
-	/**
-	 * @param employeeEmail the employeeEmail to set
-	 */
 	public void setEmployeeEmail(String employeeEmail) {
 		this.employeeEmail = employeeEmail;
 	}
 
-	/**
-	 * @return the photoId
-	 */
 	public int getPhotoId() {
 		return photoId;
 	}
 
-	/**
-	 * @param photoId the photoId to set
-	 */
 	public void setPhotoId(int photoId) {
 		this.photoId = photoId;
 	}
 
-	/**
-	 * @return the photoFilename
-	 */
 	public String getPhotoFilename() {
 		return photoFilename;
 	}
 
-	/**
-	 * @param photoFilename the photoFilename to set
-	 */
 	public void setPhotoFilename(String photoFilename) {
 		this.photoFilename = photoFilename;
 	}
 
-	/**
-	 * @param employeeId
-	 * @param countryId
-	 * @param companyId
-	 * @param firstName
-	 * @param lastName
-	 * @param birthDate
-	 * @param employeeEmail
-	 * @param photoId
-	 * @param photoFilename
-	 */
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
 	public EmployeeAndEmployeePhotosDTO(int employeeId, int countryId, int companyId, String firstName, String lastName,
-			Date birthDate, String employeeEmail, int photoId, String photoFilename) {
+			Date birthDate, String employeeEmail, int photoId, String photoFilename, boolean admin) {
 		super();
 		this.employeeId = employeeId;
 		this.countryId = countryId;
@@ -170,11 +115,9 @@ public class EmployeeAndEmployeePhotosDTO {
 		this.employeeEmail = employeeEmail;
 		this.photoId = photoId;
 		this.photoFilename = photoFilename;
+		this.admin = admin;
 	}
 
-	/**
-	 * 
-	 */
 	public EmployeeAndEmployeePhotosDTO() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -185,7 +128,10 @@ public class EmployeeAndEmployeePhotosDTO {
 		return "EmployeeAndEmployeePhotosDTO [employeeId=" + employeeId + ", countryId=" + countryId + ", companyId="
 				+ companyId + ", firstName=" + firstName + ", lastName=" + lastName + ", birthDate=" + birthDate
 				+ ", employeeEmail=" + employeeEmail + ", photoId=" + photoId + ", photoFilename=" + photoFilename
-				+ "]";
+				+ ", admin=" + admin + "]";
 	}
+
+	
+	
 
 }

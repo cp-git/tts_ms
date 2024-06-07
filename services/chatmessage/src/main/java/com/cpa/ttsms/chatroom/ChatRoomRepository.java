c89d2af2c5bfa31ee062b4a0539bd82c152f9221
Shradha2105 <shradha.p@cloudpointamerica.com>
2024-06-07 16:48:17 +0530

    chatmessage bug

--- services/chatmessage/src/main/java/com/cpa/ttsms/chatroom/ChatRoomRepository.java
+++ services/chatmessage/src/main/java/com/cpa/ttsms/chatroom/ChatRoomRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
     Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
     
   

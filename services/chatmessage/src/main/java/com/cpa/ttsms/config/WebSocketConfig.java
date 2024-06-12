434bfd5b070dc7b05cc7d771380564f49bdca8bb
Shradha2105 <86282238+Shradha2105@users.noreply.github.com>
2024-06-12 18:38:15 +0530

    Update WebSocketConfig.java

--- services/chatmessage/src/main/java/com/cpa/ttsms/config/WebSocketConfig.java
+++ services/chatmessage/src/main/java/com/cpa/ttsms/config/WebSocketConfig.java
@@ -53,7 +53,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
  
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://localhost:4200")
+        registry.addEndpoint("/ws").setAllowedOrigins("https://localhost:8443")
                 .withSockJS()
               
 ;
